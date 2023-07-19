209826ca4b4bddaf58a1fda63e95014aa3d576fb
Ravuri Teja Nagaswi <111522542+Tejaswi0418@users.noreply.github.com>
2023-07-19 12:52:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function bulboff()
+{
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1KKiw37bo5x7M3Kf1j-BK4m4yKEGym3H9gQ&usqp=CAU";
+}
+function bulbon()
+{
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTL_KxzzjSVsJ53ca6S_ytioVNv9iIJ_g8HA&usqp=CAU";
+}
